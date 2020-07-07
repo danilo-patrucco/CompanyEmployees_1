42d8a2e864f9e64b52be0c55dd7b56bf9535c40f
danilo patrucco <danilo.patrucco@gmail.com>
2020-07-07 15:39:38 -0400

    added production json, modified the startup to handle IIS and created extension folder

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,18 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+/* this class is mandatory in a ASP.NET core web API project !*/
 
 namespace CompanyEmployees
 {
@@ -25,6 +28,10 @@ namespace CompanyEmployees
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureCors();
+            services.ConfigureIISIntegration();
+            /* added the two services to support CORS and IIS integration*/
+
             services.AddControllers();
         }
 
@@ -37,6 +44,17 @@ namespace CompanyEmployees
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            /*app.UseStaticFiles() enables using static files for the request. If
+             * we don􀂶t set a path to the static files directory, it will use a wwwroot 
+             * folder in our project by default.*/
+            app.UseCors("CorsPolicy");
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            /*app.UseForwardedHeaders() will forward proxy headers to the
+             * current request. This will help us during application deployment.*/
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
 
             app.UseRouting();
 
